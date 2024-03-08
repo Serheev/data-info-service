d2fc009b16c4fde0a7c9f6f222480ce02cdfb2a1
Serheev <yserheev@gmail.com>
2024-03-08 13:10:29 -0800

    Initial commit

---
+++ src/main/java/com/serheev/datainfoservice/DataInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.serheev.datainfoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataInfoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataInfoServiceApplication.class, args);
+    }
+
+}
